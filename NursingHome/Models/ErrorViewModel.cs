c5af69010c3d8aa166a5277f2ef9c4d36e232e0c
cpatil <c.patil@bm-systems.in>
2024-07-16 12:01:01 +0530

    Add project files.

---
+++ NursingHome/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace NursingHome.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
